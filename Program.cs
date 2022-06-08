f86571155afd07f6a6a2ab7aa85298a38e0ca1b1
Marcello-Cruz <marcellinhopastelzinho@gmail.com>
2022-06-08 20:30:30 -0300

    corrige temporariamente

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Graus CF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+double temperaturaC, temperaturaF;
+Console.Writeline("Qual a temperatura C:");
+temperaturaC = Convert.ToDouble(Console.Readline())
+temperaturaF = temperaturaC * 1.8 + 32
+Console.Writeline($"{temperaturaC}C = {temperaturaF}F");
+        }
+    }
+}
